3cf92287978a07b97cb50722898ed0b15b838cfb
moschidisg <mosxidismosxidis@gmail.com>
2022-11-30 23:50:24 +0200

    add background video

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './styles/App.css';
 import Header from './components/Header';
+import bmw from './images/bmw.mp4';
 
 function App() {
     return (
         <div>
             <Header />
+            <video className="video" autoPlay loop muted>
+                <source src={bmw} type="video/mp4" />
+            </video>
         </div>
     );
 }
