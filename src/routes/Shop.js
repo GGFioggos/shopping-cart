be456321b81475b8f084ed875a986978cdd4c823
moschidisg <mosxidismosxidis@gmail.com>
2022-12-02 00:10:26 +0200

    add cart functions

--- src/routes/Shop.js
+++ src/routes/Shop.js
@@ -1,12 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import '../styles/Shop.css';
 import CarsDisplay from '../components/CarsDisplay';
 const Shop = () => {
+    const [cart, setCart] = useState([]);
+    const [itemsInCart, setItemsInCart] = useState(0);
+
+    const addToCart = (item) => {
+        setCart([...cart, item]);
+    };
+
+    useEffect(() => {
+        setItemsInCart(cart.length);
+    }, [cart]);
+
     return (
         <div>
-            <Header />
-            <CarsDisplay />
+            <Header itemsInCart={itemsInCart} />
+            <CarsDisplay addToCart={addToCart} />
         </div>
     );
 };
