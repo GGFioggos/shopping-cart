1e8db9dbab24856323783194f43f0c3b239109f2
moschidisg <mosxidismosxidis@gmail.com>
2022-12-04 01:15:35 +0200

    update cart functionalities

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Cart.css';
 import CartItem from './CartItem';
+import close from '../images/close.png';
 
 const Cart = (props) => {
     const cart = props.cart;
     const increaseQuantity = props.increaseQuantity;
     const decreaseQuantity = props.decreaseQuantity;
+    const handleClick = props.handleClick;
+    const isCartShown = props.isCartShown;
+    const total = props.total;
+
+    let empty = cart.length > 0 ? '' : 'Cart is empty';
 
     return (
-        <div className="cart">
+        <div
+            className="cart"
+            style={{
+                visibility: isCartShown ? 'visible' : 'hidden',
+                transform: isCartShown
+                    ? 'translate(-36vw,0)'
+                    : 'translate(36vw,0)',
+            }}
+        >
             <div className="cart-title">Your Cart</div>
+            <button onClick={handleClick} className="close-cart">
+                <img src={close} alt="close cart"></img>
+            </button>
+            <div className="empty-message">{empty}</div>
             {cart.map((car, index) => {
                 return (
                     <CartItem
@@ -20,6 +38,10 @@ const Cart = (props) => {
                     ></CartItem>
                 );
             })}
+            <div className="total">Total: {total}$</div>
+            <div className="checkout">
+                <button>CHECKOUT</button>
+            </div>
         </div>
     );
 };
